72214a4933e209ac3c570b594e36c5ad4ec4e42a
raphael08 <rsiphael@gmail.com>
2023-07-21 00:38:21 +0300

    .

--- Hotel/settings.py
+++ Hotel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'uosc%n-5@wr_v0j!lgi#apcfem17&3-r%$^@ohppfbf)(rm5dd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
